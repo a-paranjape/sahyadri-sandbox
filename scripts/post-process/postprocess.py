fbc0a3a203d1de5d98c59bceb570387a599b6b4b
a-paranjape <aseem@iucaa.in>
2024-09-26 17:32:09 +0530

    untested update with added support for multiprocessing

--- scripts/post-process/postprocess.py
+++ scripts/post-process/postprocess.py
@@ -5,8 +5,9 @@ from readers import SnapshotReader,HaloReader
 from correlations import PowerSpectrum
 from addvalue import AddValue
 from time import time
+import multiprocessing as mp
 
-if(len(sys.argv)==8):
+if(len(sys.argv)==9):
     sim_stem = sys.argv[1]
     snap_start = int(sys.argv[2])
     snap_end = int(sys.argv[3])
@@ -14,6 +15,7 @@ if(len(sys.argv)==8):
     grid = int(sys.argv[5])
     downsample = int(sys.argv[6])
     Lbox = float(sys.argv[7])
+    N_Proc = int(sys.argv[8])
 else: 
     sim_stem = input("Specify path (e.g., `su128/delta0.0' or 'scm1024'): ")
     snap_start = int(input("Specify starting snapshot (e.g., 0-200): "))
@@ -22,6 +24,7 @@ else:
     grid = int(input("Specify 1-d gridsize (e.g. 256) for density field calculation: "))
     downsample = int(input("Specify 1-d n_particles (e.g. 128) for downsampling; 0 to use full sample: "))
     Lbox = float(input("Specify box size (Mpc/h): "))
+    N_Proc = int(input("Specify number of cores (e.g., 16; use 1 for serial job): "))
 
 if snap_end < snap_start:
     raise ValueError("Need snap_start <= snap_end.")
@@ -30,11 +33,12 @@ logfile = None #'log_'+sim_stem+'_r{0:d}'.format(real)+'_grid{0:d}'.format(grid)
 ps = PowerSpectrum(grid=grid,Lbox=Lbox,logfile=logfile)
 
 
-calc_Pk = True
-calc_mf = True
-add_value = True
-
+###########################################
 # move these hard-coded values to a file
+calc_Pk = True
+calc_mf = False
+add_value = False
+
 if calc_Pk:
     Seed = 42
 if calc_Pk | calc_mf:
@@ -42,11 +46,22 @@ if calc_Pk | calc_mf:
     QE = 0.5
 if calc_mf:
     tags = ['mvir','m200b','m200c']
+    lgmmin = 11.0 # hard-coded to enable easier comparison across cosmologies
+    dlgm = 0.1
 if add_value:
     kmax = 0.1
+###########################################
 
-for snap in range(snap_start,snap_end+1):
-    start_time = time()
+
+if calc_mf:
+    ntags = len(tags)
+    mass_string = ''
+    for t in tags[:-1]: mass_string += t+','
+    mass_string += tags[-1]
+
+
+def do_this_snap(snap):
+    start_time_snap = time()
     sr = SnapshotReader(sim_stem=sim_stem,real=real,snap=snap,logfile=logfile)
     if calc_Pk:
         pos = sr.read_block('pos',down_to=downsample,seed=Seed)
@@ -92,14 +107,7 @@ for snap in range(snap_start,snap_end+1):
             sr.write_to_file(outfile_Pk,[ps.ktab[k],Pk_mm[k],Pk_hh[k],Pk_hm[k]])
 
     if calc_mf:
-        ntags = len(tags)
-        mass_string = ''
-        for t in tags[:-1]: mass_string += t+','
-        mass_string += tags[-1]
-
-        lgmmin = 11.0 #np.log10(mmin) # hard-code to enable easier comparison across cosmologies
         lgmmax = np.log10(hr.MHALO_MAX)
-        dlgm = 0.1
         nlgm = int((lgmmax-lgmmin)/dlgm)
         mbins = np.logspace(lgmmin,lgmmax,nlgm+1)
         mcenter = np.sqrt(mbins[1:]*mbins[:-1])
@@ -144,6 +152,126 @@ for snap in range(snap_start,snap_end+1):
         del va
     gc.collect()
     sr.print_this('... snap {0:d} done'.format(snap),sr.logfile)
+    sr.time_this(start_time_snap)
+    
+    return
+
+
+start_time = time()
+if N_Proc==1:
+    start_time = time()
+    print('serial calculation')
+    for snap in range(snap_start,snap_end+1):
+        do_this_snap(snap)
+else:
+    pool = mp.Pool(processes=N_Proc)
+    print('pool pooled')
+    results = [pool.apply_async(do_this_snap,args=(snap,)) for snap in range(snap_start,snap_end+1)]
+    print('results setup')
+    for s in range(len(results)):
+        results[s].get()
+    print('results got')
+    pool.close()
+    pool.join()
+    print('pool closed')
 
 ps.print_this('... all done',ps.logfile)
 ps.time_this(start_time)
+
+# OLDER, SERIAL CODE
+# for snap in range(snap_start,snap_end+1):
+#     start_time_snap = time()
+#     sr = SnapshotReader(sim_stem=sim_stem,real=real,snap=snap,logfile=logfile)
+#     if calc_Pk:
+#         pos = sr.read_block('pos',down_to=downsample,seed=Seed)
+#         # vel = sr.read_block('vel',down_to=downsample,seed=Seed)
+#         # ids = sr.read_block('ids',down_to=downsample,seed=Seed)
+
+#     if calc_Pk | calc_mf:
+#         hr = HaloReader(sim_stem=sim_stem,real=real,snap=snap,logfile=logfile)
+#         Npmin = hr.calc_Npmin_default(grid)
+
+#         mmin = hr.mpart*Npmin
+#         hpos,halos = hr.prep_halos(massdef=massdef,QE=QE,Npmin=Npmin)
+
+#     if calc_Pk:
+#         delta_dm = ps.density_field(pos)
+#         FT_delta_dm = ps.fourier_transform_density(delta_dm)
+#         Pk_mm = ps.Pk_grid(FT_delta_dm,input_is_FTdensity=True)
+
+#         delta_h = ps.density_field(hpos.T)
+#         FT_delta_h = ps.fourier_transform_density(delta_h)
+#         Pk_hh = ps.Pk_grid(FT_delta_h,input_is_FTdensity=True)
+#         Pk_hm = ps.Pk_grid(FT_delta_h,input_array2=FT_delta_dm,input_is_FTdensity=True)
+
+#         Pk_hh -= ps.Lbox**3/(halos.size + ps.TINY)
+#         if downsample > 0:
+#             Pk_mm -= ps.Lbox**3/(downsample**3)
+#         else:
+#             Pk_mm -= ps.Lbox**3/(sr.npart)
+
+#         sr.print_this('... done',ps.logfile)
+
+#         outfile_Pk = sr.sim_path + sim_stem + '/r'+str(real)+'/Pk_{0:03d}.txt'.format(snap)
+#         sr.print_this('Writing to file: '+outfile_Pk,sr.logfile)
+#         f = open(outfile_Pk,'w')
+#         f.write("# P(k) (DM,halos,cross) from snapshot_{0:03d}\n".format(snap))
+#         down = downsample if downsample > 0 else np.rint(sr.npart**(1/3.)).astype(int)
+#         f.write("# grid = {0:d}; downsampled to ({1:d})^3 particles\n".format(grid,down))
+#         f.write("# Halos satisfy {0:.2f} < 2T/|U| < {1:.2f}\n".format(1.-QE,1.+QE))
+#         f.write("# "+massdef+" > {0:.4e} Msun/h\n".format(mmin))
+#         f.write("# k (h/Mpc) | P(k) (Mpc/h)^3 | Phalo | Pcross\n")
+#         f.close()
+#         for k in range(ps.ktab.size):
+#             sr.write_to_file(outfile_Pk,[ps.ktab[k],Pk_mm[k],Pk_hh[k],Pk_hm[k]])
+
+#     if calc_mf:
+#         lgmmax = np.log10(hr.MHALO_MAX)
+#         nlgm = int((lgmmax-lgmmin)/dlgm)
+#         mbins = np.logspace(lgmmin,lgmmax,nlgm+1)
+#         mcenter = np.sqrt(mbins[1:]*mbins[:-1])
+#         dlnm = np.log(mbins[1]/mbins[0])
+
+#         dndlnm = np.zeros((ntags,nlgm),dtype=float)
+#         Vbox = sr.Lbox**3
+#         for t in range(ntags):
+#             tag = tags[t]
+#             dndlnm[t],temp = np.histogram(halos[tag],bins=mbins,density=False)
+#             dndlnm[t] = dndlnm[t]/dlnm/Vbox
+#             if tag==massdef:
+#                 sr.print_this("Nhalos: direct = {0:d}; integrated = {1:.1f}"
+#                               .format(halos.size,Vbox*dlnm*np.sum(dndlnm[t])),sr.logfile)
+
+#         outfile_mf = sr.halo_path + sim_stem + '/r'+str(real)+'/mf_{0:d}.txt'.format(snap)
+#         sr.print_this('Writing to file: '+outfile_mf,sr.logfile)
+#         fh = open(outfile_mf,'w')
+#         fh.write("#\n# Mass functions for " + sim_stem + '/'+'r'+str(real)+'/out_' + str(snap)+"\n")
+#         fh.write("# This file contains dn/dlnm (h/Mpc)^3 for various mass definitions.\n")
+#         fh.write("#\n# mass (Msun/h) | dndlnm["+mass_string+"]\n")
+#         fh.close()
+#         for m in range(nlgm):
+#             mlist = [mcenter[m]]
+#             for t in range(ntags):
+#                 mlist.append(dndlnm[t,m])
+#             sr.write_to_file(outfile_mf,mlist)
+
+#     if add_value:
+#         # more inclusive catalog for .vahc file
+#         hpos,halos = hr.prep_halos(QE=None,Npmin=0.0,keep_subhalos=True)
+#         av = AddValue(sim_stem=sim_stem,real=real,snap=snap,grid=grid,kmax=kmax,massdef=massdef,
+#                       ps=ps,density=FT_delta_dm,hpos=hpos,halos=halos,input_is_FTdensity=True)
+#         va = av.add_value(write_vahc=True)
+
+#     if calc_Pk:
+#         del pos#,vel,ids
+#         del delta_dm, FT_delta_dm,delta_h,FT_delta_h    
+#     if calc_Pk | calc_mf:
+#         del hpos,halos
+#     if add_value:
+#         del va
+#     gc.collect()
+#     sr.print_this('... snap {0:d} done'.format(snap),sr.logfile)
+#     sr.time_this(start_time_snap)
+
+# ps.print_this('... all done',ps.logfile)
+# ps.time_this(start_time)
