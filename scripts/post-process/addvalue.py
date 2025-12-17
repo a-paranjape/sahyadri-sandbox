16b60603ae8346ed1fc4c9fce39f21633fff42b6
Saee <63866161+SaeeDhawalikar@users.noreply.github.com>
2025-12-17 15:14:21 +0530

    added option to save the whole tensor

--- scripts/post-process/addvalue.py
+++ scripts/post-process/addvalue.py
@@ -52,43 +52,73 @@ class AddValue(HaloReader):
                 
 
     ###############################################
-    def add_value(self,write_vahc=False):
+    def add_value(self,write_vahc=False, only_tensor=False):
         """ Given matter (Fourier) density field, halo positions and halo properties, 
             along with instance powspec of PowerSpectrum(),
             calculate array of value-added quantities and optionally write .vahc file. 
+            if only_tensor is True, stores the tidal tensor in a .tens file
         """
-        va_props = np.zeros(self.Nhalo,dtype=self.vadtypelist)
+
+        if only_tensor:
+            out_dtype = [('ID', 'i8')]
+            tstr = 'tens'
+            for s in self.scale_strings:
+                out_dtype.extend([(tstr+'11_'+s,'f'),(tstr+'22_'+s,'f'),(tstr+'33_'+s,'f'),(tstr+'12_'+s,'f'),(tstr+'13_'+s,'f'),(tstr+'23_'+s,'f')])
+            va_props = np.zeros(self.Nhalo,dtype=out_dtype)
+        else:
+            va_props = np.zeros(self.Nhalo,dtype=self.vadtypelist)
         va_props['ID'] = self.halos['ID']
 
-        va_props['b1'] = self.calc_b1()
+        if only_tensor is False:
+            va_props['b1'] = self.calc_b1()
 
-        out = self.calc_tensor(tensor='tidal')
-        for s in self.scale_strings:
-            va_props['lam1_'+s] = out['lam1_'+s] 
-            va_props['lam2_'+s] = out['lam2_'+s] 
-            va_props['lam3_'+s] = out['lam3_'+s]
-            
-        out = self.calc_tensor(tensor='hessian')
-        for s in self.scale_strings[-2:]:
-            va_props['lamH1_'+s] = out['lamH1_'+s] 
-            va_props['lamH2_'+s] = out['lamH2_'+s] 
-            va_props['lamH3_'+s] = out['lamH3_'+s]
-            
+        out = self.calc_tensor(tensor='tidal', only_tensor=only_tensor)
+
+        if only_tensor is False:
+            for s in self.scale_strings:
+                va_props['lam1_'+s] = out['lam1_'+s]
+                va_props['lam2_'+s] = out['lam2_'+s]
+                va_props['lam3_'+s] = out['lam3_'+s]
+
+            out = self.calc_tensor(tensor='hessian', only_tensor=only_tensor)
+            for s in self.scale_strings[-2:]:
+                va_props['lamH1_'+s] = out['lamH1_'+s]
+                va_props['lamH2_'+s] = out['lamH2_'+s]
+                va_props['lamH3_'+s] = out['lamH3_'+s]
+        else:
+            for s in self.scale_strings:
+                va_props['tens11_'+s] = out['tens11_'+s]
+                va_props['tens22_'+s] = out['tens22_'+s]
+                va_props['tens33_'+s] = out['tens33_'+s]
+                va_props['tens12_'+s] = out['tens12_'+s]
+                va_props['tens13_'+s] = out['tens13_'+s]
+                va_props['tens23_'+s] = out['tens23_'+s]
+
         del out
         gc.collect()
 
         if write_vahc:
-            va_cat = self.sim_stem + '/r{0:d}/out_{1:d}.vahc'.format(self.real,self.snap)
+            if (only_tensor is False):
+                va_cat = self.sim_stem + '/r{0:d}/out_{1:d}.vahc'.format(self.real,self.snap)
+            else:
+                va_cat = self.sim_stem + '/r{0:d}/out_{1:d}.tens'.format(self.real,self.snap)
+
             if self.verbose:
                 self.print_this('Writing to file: '+ va_cat,self.logfile)
             va_cat = self.halo_path + va_cat
             header_string =  "# Value added halo catalog. All quantities computed on {0:d}^3 grid\n".format(self.grid)
             header_string += '# haloID'
-            for s in self.scale_strings:
-                header_string += ' lam1_'+s+' lam2_'+s+' lam3_'+s
-            for s in self.scale_strings[-2:]:
-                header_string += ' lamH1_'+s+' lamH2_'+s+' lamH3_'+s
-            header_string += ' b1' # deleted old 'b1', renamed old 'b1wtd' as new 'b1'
+
+            if only_tensor is False:
+                for s in self.scale_strings:
+                    header_string += ' lam1_'+s+' lam2_'+s+' lam3_'+s
+                for s in self.scale_strings[-2:]:
+                    header_string += ' lamH1_'+s+' lamH2_'+s+' lamH3_'+s
+                header_string += ' b1' # deleted old 'b1', renamed old 'b1wtd' as new 'b1'
+            else:
+                for s in self.scale_strings:
+                    header_string += ' tens11_'+s+' tens22_'+s+' tens33_'+s+' tens12_'+s+' tens13_'+s+' tens23_'+s
+
             header_string += '\n'
             f = open(va_cat,'w')
             f.write(header_string)
@@ -101,6 +131,7 @@ class AddValue(HaloReader):
 
     ###############################################
     def calc_b1(self):
+        print("\n\n, in addvalue.py, we are calculating calc_b1\n")
         out = np.zeros(self.Nhalo)
         RANGE_MAX = np.max([1,np.rint(self.Nhalo/self.NTRC_SPLIT*(self.kmax - self.ps.Delta_k)/(0.1 - 2*np.pi/300)).astype(int)])
         if RANGE_MAX == 1:
@@ -111,8 +142,13 @@ class AddValue(HaloReader):
         else:
             if self.verbose:
                 self.print_this("... ... splitting tracers into {0:d} ranges".format(RANGE_MAX),self.logfile)
+                print("RANGE_MAX:", RANGE_MAX)
             for r in range(RANGE_MAX):
-                sl = np.s_[r*self.Nhalo/RANGE_MAX:(r+1)*self.Nhalo/RANGE_MAX]
+                print(r, "calculating sl")
+                print(self.Nhalo)
+                sl = np.s_[r*self.Nhalo//RANGE_MAX:(r+1)*self.Nhalo//RANGE_MAX]
+                print("sl", sl)
+                print(np.shape(sl))
                 b1_tmp,b1_k,ktab_trunc = self.ps.halobyhalo_b1(self.density,self.hpos[sl],kmax=self.kmax,
                                                                input_is_density=True,input_is_FTdensity=self.iFTd,CIC=self.CIC)
                 out[sl] = b1_tmp 
@@ -125,7 +161,8 @@ class AddValue(HaloReader):
 
     
     ###############################################
-    def calc_tensor(self,tensor='tidal'):
+    def calc_tensor(self,tensor='tidal', only_tensor=False):
+        """ if only_tensor=True, onlyt the tensor is calculated, and not its eigenvalues"""
         if tensor not in ['tidal','hessian']:
             raise ValueError("kwarg tensor should be one of ['tidal','hessian'] in calc_tensor().")
         allowed_h = np.where(self.halos[self.massdef] > self.mmin)[0]
@@ -134,7 +171,11 @@ class AddValue(HaloReader):
         N_scales_this = self.N_scales if tensor == 'tidal' else 2
         smoothing_scales_this = self.smoothing_scales if tensor == 'tidal' else self.smoothing_scales[-2:]
         scale_strings_this = self.scale_strings if tensor == 'tidal' else self.scale_strings[-2:]
-        tens_eigvals = np.zeros((self.Nhalo,N_scales_this,3),dtype=float)
+        if (only_tensor is False):
+            tens_eigvals = np.zeros((self.Nhalo,N_scales_this,3),dtype=float)
+        else:
+            OUT = np.zeros([self.Nhalo, N_scales_this, 6], dtype=float) # 6 for the independent components of the tidal tensor
+
         
         if self.verbose:
             self.print_this("... ignoring halos smaller than mmin = {0:.3e}Msun/h for tidal tensor".format(self.mmin),self.logfile)
@@ -163,12 +204,16 @@ class AddValue(HaloReader):
                 tens13_h = tens13[hcell[0],hcell[1],hcell[2]].real
                 tens23_h = tens23[hcell[0],hcell[1],hcell[2]].real
 
-                tens_mat = np.matrix(np.zeros((3,3),dtype=float))
-                tens_mat[np.diag_indices(3)]    = np.array([tens11_h,tens22_h,tens33_h])
-                tens_mat[np.triu_indices(3,1)]  = np.array([tens12_h,tens13_h,tens23_h])
-                tens_mat[np.tril_indices(3,-1)] = np.array([tens12_h,tens13_h,tens23_h])
+                if (only_tensor is True):
+                    OUT[h, s]=np.array([tens11_h, tens22_h, tens33_h, tens12_h, tens13_h, tens23_h])
+                else:
+                    tens_mat = np.matrix(np.zeros((3,3),dtype=float))
+                    tens_mat[np.diag_indices(3)]    = np.array([tens11_h,tens22_h,tens33_h])
+                    tens_mat[np.triu_indices(3,1)]  = np.array([tens12_h,tens13_h,tens23_h])
+                    tens_mat[np.tril_indices(3,-1)] = np.array([tens12_h,tens13_h,tens23_h])
 
-                tens_eigvals[h,s] = linalg.eigvalsh(tens_mat)
+                    tens_eigvals[h,s] = linalg.eigvalsh(tens_mat)
+
                 if self.verbose:
                     self.status_bar(cnt,N_allowed)
                 cnt += 1
@@ -176,48 +221,93 @@ class AddValue(HaloReader):
             del tens11,tens22,tens33,tens12,tens13,tens23
             gc.collect()
 
-
-        tens_eigvals = np.transpose(tens_eigvals,(2,0,1))
-        # shape (3,Nhalo,N_scales)
-        
-        tens_eigvals_final = np.zeros((len(scale_strings_this),3,self.Nhalo),dtype=float)
-
-        last_start = -3 if tensor == 'tidal' else -2
-        for s in range(last_start,0):
-            tens_eigvals_final[s] = tens_eigvals[:,:,s].copy()
-
-        if tensor == 'tidal':
-            if self.verbose:
-                self.print_this('... interpolating to Nx R200b for all halos',self.logfile)
-            for s in range(self.N_scalestrings-3):
-                if self.verbose:
-                    self.print_this("... ... {0:d}x R200b".format(2*(s+1)),self.logfile)
-                RGeff = (s+1)*self.RG_2R200b_NORM*((self.halos[self.massdef]/self.M_NORM)*(self.OM_DEF/self.Om))**(1/3.) 
-                # works because s=0,1,2,3 so s+1=1x,2x,3x,4x base value
-                self.print_this('... ... ... looping over halos',self.logfile)
-                cnt = 0
-                for h in allowed_h:
-                    for eig in range(3):
-                        tens_eigvals_final[s,eig,h] = np.interp(np.log10(RGeff[h]),np.log10(self.RGtab),tens_eigvals[eig,h,:self.N_RG])
-                    if self.verbose:
-                        self.status_bar(cnt,N_allowed)
-                    cnt += 1
-
-                
-        out_dtype = []
-        tstr = 'lam'
-        if tensor == 'hessian':
-            tstr += 'H'
-        for s in scale_strings_this:
-            out_dtype.extend([(tstr+'1_'+s,'f'),(tstr+'2_'+s,'f'),(tstr+'3_'+s,'f')])
-        out = np.zeros(self.Nhalo,dtype=out_dtype)
+        if (only_tensor is False):
+            tens_eigvals = np.transpose(tens_eigvals,(2,0,1))
+            # shape (3,Nhalo,N_scales)
             
-        for s in range(len(scale_strings_this)):
-            out[tstr+'1_'+scale_strings_this[s]] = tens_eigvals_final[s,0]
-            out[tstr+'2_'+scale_strings_this[s]] = tens_eigvals_final[s,1]
-            out[tstr+'3_'+scale_strings_this[s]] = tens_eigvals_final[s,2]
+            tens_eigvals_final = np.zeros((len(scale_strings_this),3,self.Nhalo),dtype=float)
 
-        del tens_eigvals,tens_eigvals_final
+            last_start = -3 if tensor == 'tidal' else -2
+            for s in range(last_start,0):
+                tens_eigvals_final[s] = tens_eigvals[:,:,s].copy()
+
+            if tensor == 'tidal':
+                if self.verbose:
+                    self.print_this('... interpolating to Nx R200b for all halos',self.logfile)
+                for s in range(self.N_scalestrings-3):
+                    if self.verbose:
+                        self.print_this("... ... {0:d}x R200b".format(2*(s+1)),self.logfile)
+                    RGeff = (s+1)*self.RG_2R200b_NORM*((self.halos[self.massdef]/self.M_NORM)*(self.OM_DEF/self.Om))**(1/3.) 
+                    # works because s=0,1,2,3 so s+1=1x,2x,3x,4x base value
+                    self.print_this('... ... ... looping over halos',self.logfile)
+                    cnt = 0
+                    for h in allowed_h:
+                        for eig in range(3):
+                            tens_eigvals_final[s,eig,h] = np.interp(np.log10(RGeff[h]),np.log10(self.RGtab),tens_eigvals[eig,h,:self.N_RG])
+                        if self.verbose:
+                            self.status_bar(cnt,N_allowed)
+                        cnt += 1
+
+                    
+            out_dtype = []
+            tstr = 'lam'
+            if tensor == 'hessian':
+                tstr += 'H'
+            for s in scale_strings_this:
+                out_dtype.extend([(tstr+'1_'+s,'f'),(tstr+'2_'+s,'f'),(tstr+'3_'+s,'f')])
+            out = np.zeros(self.Nhalo,dtype=out_dtype)
+                
+            for s in range(len(scale_strings_this)):
+                out[tstr+'1_'+scale_strings_this[s]] = tens_eigvals_final[s,0]
+                out[tstr+'2_'+scale_strings_this[s]] = tens_eigvals_final[s,1]
+                out[tstr+'3_'+scale_strings_this[s]] = tens_eigvals_final[s,2]
+
+            del tens_eigvals,tens_eigvals_final
+        else:
+            OUT = np.transpose(OUT,(2,0,1))
+            # shape (6,Nhalo,N_scales)
+            tens_final = np.zeros((len(scale_strings_this),6,self.Nhalo),dtype=float)
+
+            last_start = -3 if tensor == 'tidal' else -2
+            for s in range(last_start,0):
+                tens_final[s] = OUT[:,:,s].copy()
+
+            if tensor == 'tidal':
+                if self.verbose:
+                    self.print_this('... interpolating to Nx R200b for all halos',self.logfile)
+                for s in range(self.N_scalestrings-3):
+                    if self.verbose:
+                        self.print_this("... ... {0:d}x R200b".format(2*(s+1)),self.logfile)
+                    RGeff = (s+1)*self.RG_2R200b_NORM*((self.halos[self.massdef]/self.M_NORM)*(self.OM_DEF/self.Om))**(1/3.)
+                    # works because s=0,1,2,3 so s+1=1x,2x,3x,4x base value
+                    self.print_this('... ... ... looping over halos',self.logfile)
+                    cnt = 0
+                    for h in allowed_h:
+                        for eig in range(6):
+                            tens_final[s,eig,h] = np.interp(np.log10(RGeff[h]),np.log10(self.RGtab),OUT[eig,h,:self.N_RG])
+                        if self.verbose:
+                            self.status_bar(cnt,N_allowed)
+                        cnt += 1
+
+
+            out_dtype = []
+            tstr = 'tens'
+            if tensor == 'hessian':
+                tstr += 'H'
+            for s in scale_strings_this:
+                out_dtype.extend([(tstr+'11_'+s,'f'),(tstr+'22_'+s,'f'),(tstr+'33_'+s,'f'),(tstr+'12_'+s,'f'),(tstr+'13_'+s,'f'),(tstr+'23_'+s,'f')])
+            out = np.zeros(self.Nhalo,dtype=out_dtype)
+
+            for s in range(len(scale_strings_this)):
+                out[tstr+'11_'+scale_strings_this[s]] = tens_final[s,0]
+                out[tstr+'22_'+scale_strings_this[s]] = tens_final[s,1]
+                out[tstr+'33_'+scale_strings_this[s]] = tens_final[s,2]
+                out[tstr+'12_'+scale_strings_this[s]] = tens_final[s,3]
+                out[tstr+'13_'+scale_strings_this[s]] = tens_final[s,4]
+                out[tstr+'23_'+scale_strings_this[s]] = tens_final[s,5]
+
+            del tens_final, OUT
+
         gc.collect()
         
         return out
